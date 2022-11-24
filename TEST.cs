eb389f4d0a2a1ecfdea85d60e9cf4e569719e4d2
Abhishek.Kumar5 <abhishek.kumar5@citiustech.com>
2022-11-24 18:40:05 +0530

    Add project files.

---
+++ TEST.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DotnetTricks
+{
+    class TEST
+    {
+        public static int sumnumbers(int x, int y)
+        {
+        return x+y;
+        }
+        public static void Main()
+        {
+        int x = 10;
+        int  y = 10;
+        int result = sumnumbers(x, y);
+        Console.WriteLine(result);
+
+        }
+    }
+}
